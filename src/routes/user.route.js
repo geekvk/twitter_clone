1881d60274e35edc7bfa41de32eaebb7c1b41e02
geekvk <vijanakaush@gmail.com>
2025-07-21 06:06:12 +0530

    few chages done

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/profile/:username", getUserProfile);
 
-router.post("/sync", protectRoute, syncUser);
+router.post("/sync", syncUser);
 router.post("/me", protectRoute, getCurrentUser);
 router.put("/profile", protectRoute, updateProfile);
 router.post("/follow/:targetUserId", protectRoute, followUser);
