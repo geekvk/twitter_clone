964bfe419ef26df73a075b6c24b369b48f9f1b73
geekvk <vijanakaush@gmail.com>
2025-07-21 06:02:09 +0530

    few chages done

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,10 @@
-export const protectRoute = async(req, res, next) => {
-    const { userId } = req.auth() || {};
-    if(!userId){
-        return res.status(401).json({ message : 'Forbidden'});
-    }
-    next();
-}
+export const protectRoute = (req, res, next) => {
+  const auth = getAuth(req);
+
+  if (!auth.userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  req.auth = auth; // attach to request for later use
+  next();
+};
